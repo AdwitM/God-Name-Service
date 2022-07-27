be2e3e8989ea2b06c660bf89779968c036de48be
Adwit Mittal <adwitmittal@gmail.com>
2022-07-27 19:35:05 +0530

    update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,12 +1,12 @@
 const main = async () => {
     const domainContractFactory = await hre.ethers.getContractFactory('Domains');
-    const domainContract = await domainContractFactory.deploy("ninja");
+    const domainContract = await domainContractFactory.deploy("god");
     await domainContract.deployed();
   
     console.log("Contract deployed to:", domainContract.address);
   
     // CHANGE THIS DOMAIN TO SOMETHING ELSE! I don't want to see OpenSea full of bananas lol
-    let txn = await domainContract.register("Nero",  {value: hre.ethers.utils.parseEther('0.1')});
+    let txn = await domainContract.register("Nero",  {value: hre.ethers.utils.parseEther('0.3')});
     await txn.wait();
     console.log("Minted domain Nero.god");
   
