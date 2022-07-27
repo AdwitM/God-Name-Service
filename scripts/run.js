53ae9104564cf0762e81e995e0082f0c15cd3fe4
Adwit Mittal <adwitmittal@gmail.com>
2022-07-27 12:50:51 +0530

    update run.js

--- scripts/run.js
+++ scripts/run.js
@@ -1,16 +1,21 @@
 const main = async () => {
+    // The first return is the deployer, the second is a random account
     const [owner, randomPerson] = await hre.ethers.getSigners();
     const domainContractFactory = await hre.ethers.getContractFactory('Domains');
     const domainContract = await domainContractFactory.deploy();
     await domainContract.deployed();
     console.log("Contract deployed to:", domainContract.address);
     console.log("Contract deployed by:", owner.address);
-    
-    const txn = await domainContract.register("doom");
+  
+    let txn = await domainContract.register("helo");
     await txn.wait();
   
-    const domainOwner = await domainContract.getAddress("doom");
-    console.log("Owner of domain:", domainOwner);
+    const domainAddress = await domainContract.getAddress("helo");
+    console.log("Owner of domain helo:", domainAddress);
+  
+    // Trying to set a record that doesn't belong to me!
+    txn = await domainContract.connect(randomPerson).setRecord("helo", "Haha my domain now!");
+    await txn.wait();
   }
   
   const runMain = async () => {
