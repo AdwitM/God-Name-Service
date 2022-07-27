777900537e99670d26e2880750bc6a7ec6205bb4
Adwit Mittal <adwitmittal@gmail.com>
2022-07-27 19:27:58 +0530

    changes

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,11 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 module.exports = {
-  solidity: "0.8.10",
+  solidity: "0.8.15",
   networks: {
     mumbai: {
-      url: "YOUR_ALCHEMY_MUMBAI_URL",
-      accounts: [""],
+      url: "https://polygon-mumbai.g.alchemy.com/v2/cuj10P5kwylYzK0yQ3p9oyad3KD7J3jV",
+      accounts: ["1df4c912b243fe2a0900b493b65160a38185f65c6675b476765ba39a9be78c3c"],
     }
   }
 };
